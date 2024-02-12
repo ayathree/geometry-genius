1cd252fa330fe0ef54245ad0dcad31a224f5b2b1
ayathree <nobanitaayathree333@gmail.com>
2024-02-12 22:15:23 +0600

    first commit

---
+++ scripts/triangle.js
@@ -0,0 +1,18 @@
+function calculateTriangle() {
+    // for base
+    const calculateBase = document.getElementById('base')
+    const baseValue = calculateBase.value;
+    const base = parseFloat(baseValue);
+    console.log( base);
+    // for height
+    const calculateHeight = document.getElementById('height')
+    const heightValue = calculateHeight.value;
+    const height = parseFloat(heightValue);
+    console.log(height);
+    // calculation
+    const resultTriangle = 0.5*base*height;
+    console.log(resultTriangle);
+    // show-result
+    const showTriangle = document.getElementById('calculation');
+    showTriangle.innerText = resultTriangle;
+}
