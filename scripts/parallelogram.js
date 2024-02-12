aca7a9fe8ac316a0afa6066c0f3aa2f1bded4fad
ayathree <nobanitaayathree333@gmail.com>
2024-02-13 00:39:37 +0600

    s

---
+++ scripts/parallelogram.js
@@ -0,0 +1,11 @@
+function calculationParallelogram() {
+    const base = parallelogram('base-pa')
+    
+    
+}
+
+function parallelogram(inputFieldId) {
+    const inputFieldId = document.getElementById(inputFieldId);
+    console.log(inputFieldId.value)
+    
+}
